3d06ddc9da2ac54435316c379bfca4bb3ca46b98
BStevic <bdstevic@icloud.com>
2023-04-12 13:43:12 -0600

    Begin TestServer for unit tests

---
+++ pushoverlib-tests/TestServer.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace pushoverlib_tests; 
+
+internal class TestServer {
+    public int Port { get; private set; }
+    private HttpListener listener;
+
+    public void Start() {
+        listener.Start();
+        Receive();
+    }
+
+    public void Stop() {
+        listener.Stop();
+    }
+
+    private void Receive() {
+        listener.BeginGetContext(new AsyncCallback(ListenerCallback), listener);
+    }
+
+    private void ListenerCallback(IAsyncResult result) {
+        
+    }
+
+    public TestServer(int port) {
+        this.Port = port;
+        this.listener = new HttpListener();
+        listener.Prefixes.Add("http://*:" + port + "/");
+    }
+}
