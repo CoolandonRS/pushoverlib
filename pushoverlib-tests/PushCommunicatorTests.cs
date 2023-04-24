64862b35be9f20d43f2f2fa6a6e271609338ac32
BStevic <bdstevic@icloud.com>
2023-04-24 13:46:24 -0600

    Start unit tests

---
+++ pushoverlib-tests/PushCommunicatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace pushoverlib_tests; 
+
+public class PushCommunicatorTests {
+    private TestServer server;
+    private PushData defData;
+    
+    [OneTimeSetUp]
+    public void SingleSetUp() {
+        server = new TestServer(8080);
+        PushCommunicator.TestMode(8080);
+        defData = new PushDataBuilder().Build().AddNeeded("fakeToken", "fakeToken", "msg");
+    }
+
+    [Test]
+    public void SendAsync() {
+        Assert.Multiple(async () => {
+            Respond(500);
+            Assert.ThrowsAsync(typeof(PushException), async () => {
+                await PushCommunicator.SendAsync(defData);
+            }, "Success on error code");
+            Respond(400, "{\"message\":\"cannot be blank\",\"errors\":[\"message cannot be blank\"],\"status\":0,\"request\":\"reqToken\"}");
+            Assert.That((await PushCommunicator.SendAsync(defData)).HasErrors(), Is.False, "Didn't recognize errors");
+            Assert.ThrowsAsync(typeof(PushRequestException), async () => {
+                await PushCommunicator.ConfirmSendAsync(defData);
+            }, "Errors success in ConfirmSendAsync");
+        });
+    }
+
+    private void Respond(int status, string msg = "") {
+        server.SetResponse(status, msg);
+    }
+}
--- pushoverlib-tests/TestServer.cs
+++ pushoverlib-tests/TestServer.cs
@@ -1,11 +1,23 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace pushoverlib_tests; 
 
 internal class TestServer {
     public int Port { get; private set; }
     private HttpListener listener;
+    public Response Resp { get; private set; } = new Response(500, "No response set");
+
+    public class Response {
+        public readonly int StatusCode;
+        public readonly string msg;
+
+        public Response(int statusCode, string msg = "") {
+            StatusCode = statusCode;
+            this.msg = msg;
+        }
+    }
 
     public void Start() {
         listener.Start();
@@ -21,12 +33,31 @@ internal class TestServer {
     }
 
     private void ListenerCallback(IAsyncResult result) {
+        if (!listener.IsListening) return;
         
+        var context = listener.EndGetContext(result);
+        var request = context.Request;
+        var response = context.Response;
+        response.StatusCode = Resp.StatusCode;
+        response.ContentType = "application/json";
+        response.OutputStream.Write(Encoding.UTF8.GetBytes(Resp.msg));
+        response.OutputStream.Close();
+        
+        Receive();
     }
 
-    public TestServer(int port) {
+    public void SetResponse(Response response) {
+        Resp = response;
+    }
+
+    public void SetResponse(int statusCode, string msg = "") {
+        Resp = new Response(statusCode, msg);
+    }
+    
+    public TestServer(int port, bool startOnConstruct = true) {
         this.Port = port;
         this.listener = new HttpListener();
-        listener.Prefixes.Add("http://*:" + port + "/");
+        listener.Prefixes.Add("http://127.0.0.1:" + port + "/");
+        if (startOnConstruct) Start();
     }
 }
